ab9240f2622d78aa76d85398e00e4f5566f8b509
Keenan Speaks <keenanspeaks@gmail.com>
2023-02-05 20:28:33 -0500

    test - playerSelection = rock. Single round

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 
 getComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * 3 + 1)
+    // Written in If/Else Statement
     /*if(randomNumber === 1) {
         return 'rock'
     } else if (randomNumber === 2){
@@ -22,8 +23,34 @@ getComputerChoice = () => {
     }
 } 
 
+//Check getComputerChoice function.
 console.log(getComputerChoice())
 //Write a function that plays a single round of RPS. Function should take two parameters. (playerSelection,computerSelection) and then RETURN a string that declares the winner of the round. Ex: "You Lose! Paper Beats Rock" (Make case sensitive so users can input rock/Rock or any other variation)
 
 
+playRound = (playerSelection , computerSelection) => {
+    if(playerSelection === 'rock' && computerSelection === 'rock'){
+        return 'It\'s a tie!';
+    } else if (playerSelection === 'rock' && computerSelection === 'paper'){
+        return 'You Lose! Paper Beats Rock!!!'
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You Win!!! Rock beats scissors!!!'
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return 'It\s a tie!';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You Win!!!! Paper beats rock!!!'
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You Lose! Scissors beats paper!!'
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+        return 'It\s a tie!'
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper'){
+        return 'You Win!!! Scissors beats paper!!!'
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You Lose! Rock beats scissors!'
+    }
+}
 
+const playerSelection = 'rock'
+let computerSelection = getComputerChoice()
+
+console.log(playRound(playerSelection, computerSelection))
